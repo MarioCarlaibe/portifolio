15eeccc36e94b32dbf46ac688d3566993cc2272a
“MarioCarlaibe” <mariocarlaibe@hotmail.com>
2024-12-29 11:50:05 -0300

    adiconado troca de tema, animações nos projetos e nas skills

--- src/script/index.js
+++ src/script/index.js
@@ -20,3 +20,33 @@ document.getElementById("copy-phone").addEventListener("click", function () {
   navigator.clipboard.writeText(textToCopy)
   alert("Telefone Copiado!")
 })
+function handleScroll() {
+  const work = document.querySelectorAll('.work');
+  const triggerPoint = window.innerHeight / 1.2;
+  
+  work.forEach(item => {
+    const workTop = item.getBoundingClientRect().top;
+
+    if (workTop < triggerPoint) {
+      item.classList.add('show')
+    } else {
+      item.classList.remove('show')
+    }
+  })
+}
+window.addEventListener('scroll', handleScroll)
+handleScroll()
+
+const toggleTheme = () => {
+  const html = document.documentElement;
+  
+  // Alterna entre os temas claro e escuro
+  if (html.getAttribute('data-theme') === 'dark') {
+    html.setAttribute('data-theme', 'light');
+  } else {
+    html.setAttribute('data-theme', 'dark');
+  }
+};
+
+// Adiciona um evento no botão de alternar tema
+document.querySelector('#theme').addEventListener('click', toggleTheme);
